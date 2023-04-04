50f088ba5fa01047270788562aac34c7d8df5f23
Nurlan Turganaliev <nurlan.turganaliev@gmail.com>
2023-04-04 20:23:40 +0600

    Integradte livereload

--- render_website.py
+++ render_website.py
@@ -2,27 +2,33 @@ import json
 
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+from livereload import Server
+from more_itertools import chunked
 
 
-def main():
+def on_reload():
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
     )
-
     template = env.get_template('template.html')
     with open('books/book_descriptions.json', 'r') as file:
         book_descriptions_json = file.read()
+    book_descriptions = json.loads(book_descriptions_json)
+    book_description_chunks = list(chunked(book_descriptions, 2))
 
     rendered_page = template.render(
-        book_descriptions = json.loads(book_descriptions_json)
+        book_description_chunks=book_description_chunks
     )
-
     with open('index.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
 
-    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-    server.serve_forever()
+
+def main():
+    on_reload()
+    server = Server()
+    server.watch('template.html', on_reload)
+    server.serve(root='.')
 
 
 if __name__ == '__main__':
